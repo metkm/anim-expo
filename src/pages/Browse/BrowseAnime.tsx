dfec2a68a61951fb872e3da93c07f19a572e9ea1
Sibyl <metinkorkmaz417@gmail.com>
2022-01-22 20:34:22 +0300

    big overall css changes

--- src/pages/Browse/BrowseAnime.tsx
+++ src/pages/Browse/BrowseAnime.tsx
@@ -10,7 +10,7 @@ const BrowseAnime = () => {
 
   if (!browse) return <Loading />;
   return (
-    <ScrollView style={{ paddingLeft: 10 }}>
+    <ScrollView style={{ paddingLeft: 6 }}>
       <BrowseRow title="Trending Now" mediaList={browse.trending.media} />
       <BrowseRow title="Popular This Season" mediaList={browse.season.media} />
       <BrowseRow title="Upcoming Next Season" mediaList={browse.nextSeason.media} />
