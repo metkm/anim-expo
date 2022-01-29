723224ddf1d8e5aea0f1a6e3ecaa76a957b3ee9d
Sibyl <metinkorkmaz417@gmail.com>
2022-01-29 17:39:15 +0300

    move Home page to It's own file

--- App.tsx
+++ App.tsx
@@ -1,90 +1,35 @@
 import "react-native-gesture-handler";
 import "react-native-reanimated";
 
-import React, { useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import { useColorScheme } from "react-native";
 import { Overpass_400Regular, Overpass_700Bold, useFonts } from "@expo-google-fonts/overpass";
 import AppLoading from "expo-app-loading";
 
-import { Provider, useSelector, useDispatch } from "react-redux";
-import { store, persistor, RootState } from "./src/store";
+import { Provider } from "react-redux";
+import { store, persistor } from "./src/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { asyncLogin } from "./src/store/userSlice";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator, TransitionPresets } from "@react-navigation/stack";
 
 import Character from "./src/pages/Character";
 import Activity from "./src/pages/Activity";
 import Settings from "./src/pages/Settings";
 import Notifications from "./src/pages/Notifications";
-import Library from "./src/pages/Library/Library";
-import Browse from "./src/pages/Browse/Browse";
-import Login from "./src/pages/Login";
+import Home from "./src/pages/Home";
 import Media from "./src/pages/Media";
 import User from "./src/pages/User";
 
 import { animDark, animLight } from "./src/constants/theme";
-import { useColors } from "./src/hooks/useColors";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { AppStackParamList, HomeTabParamList } from "./src/pages/props";
+import { AppStackParamList } from "./src/pages/props";
 
 axios.defaults.baseURL = "https://graphql.anilist.co";
 
-const Tab = createBottomTabNavigator<HomeTabParamList>();
 const Stack = createStackNavigator<AppStackParamList>();
-
-interface Icons {
-  [index: string]: string;
-}
-
-const Home = () => {
-  const accessToken = useSelector((state: RootState) => state.token.accessToken);
-  const user = useSelector((state: RootState) => state.user.user);
-  const dispatch = useDispatch();
-  const { color } = useColors();
-
-  useEffect(() => {
-    if (accessToken) {
-      axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
-      dispatch(asyncLogin());
-    }
-  }, [accessToken]);
-
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          const icons: Icons = {
-            User: "account",
-            Library: "book",
-            Browse: "compass",
-            Login: "login",
-          };
-
-          return <Icon name={icons[route.name]} size={size} color={color} />;
-        },
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: color,
-      })}
-    >
-      <Tab.Screen name="Browse" component={Browse} />
-      {user ? (
-        <>
-          <Tab.Screen name="Library" component={Library} initialParams={{ userId: user.id }} />
-          <Tab.Screen name="User" component={User} initialParams={{ userId: user.id }} />
-        </>
-      ) : (
-        <Tab.Screen name="Login" component={Login} />
-      )}
-    </Tab.Navigator>
-  );
-};
 
 const App = () => {
   const isDark = useColorScheme() == "dark";
---
+++ src/pages/Home.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+import { HomeTabParamList } from "./props";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { RootState } from "../store";
+import { asyncLogin } from "../store/userSlice";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useColors } from "../hooks/useColors";
+
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Browse from "./Browse/Browse";
+import Library from "./Library/Library";
+import Login from "./Login";
+import User from "./User";
+
+const Tab = createBottomTabNavigator<HomeTabParamList>();
+
+interface Icons {
+  [index: string]: string;
+}
+
+const Home = () => {
+  const accessToken = useSelector((state: RootState) => state.token.accessToken);
+  const user = useSelector((state: RootState) => state.user.user);
+  const dispatch = useDispatch();
+  const { color } = useColors();
+
+  useEffect(() => {
+    if (accessToken) {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+      dispatch(asyncLogin());
+    }
+  }, [accessToken]);
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          const icons: Icons = {
+            User: "account",
+            Library: "book",
+            Browse: "compass",
+            Login: "login",
+          };
+
+          return <Icon name={icons[route.name]} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: color,
+      })}
+    >
+      <Tab.Screen name="Browse" component={Browse} />
+      {user ? (
+        <>
+          <Tab.Screen name="Library" component={Library} initialParams={{ userId: user.id }} />
+          <Tab.Screen name="User" component={User} initialParams={{ userId: user.id }} />
+        </>
+      ) : (
+        <Tab.Screen name="Login" component={Login} />
+      )}
+    </Tab.Navigator>
+  );
+};
+
+export default Home;
