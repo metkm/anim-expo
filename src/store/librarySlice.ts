d501364e778ce94dc5bea389554f0e2504ea9963
Sibyl <metinkorkmaz417@gmail.com>
2022-02-02 21:20:13 +0300

    added librarySlice

---
+++ src/store/librarySlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { MediaListObject } from "../api/objectTypes";
+
+interface InitialState {
+  mediaList: MediaListObject[];
+  categories: string[];
+}
+
+const initialState: InitialState = {
+  mediaList: [],
+  categories: [],
+};
+
+export const librarySlice = createSlice({
+  name: "library",
+  initialState,
+  reducers: {
+    addMedia: (state, action: PayloadAction<MediaListObject>) => {
+      state.mediaList.push(action.payload);
+    },
+    removeMedia: (state, action: PayloadAction<number>) => {
+      state.mediaList = [...state.mediaList.slice(action.payload, 1)];
+    },
+    setCategories: (state, action: PayloadAction<string[]>) => {
+      state.categories = action.payload;
+    },
+  },
+});
