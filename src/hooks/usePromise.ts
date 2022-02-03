f1978971ffbb14a048e6fb9aa815245535513a03
Sibyl <metinkorkmaz417@gmail.com>
2022-02-03 23:20:55 +0300

    updated usePromise to infer parameters

--- src/hooks/usePromise.ts
+++ src/hooks/usePromise.ts
@@ -1,9 +1,9 @@
-import { useState, useCallback } from "react";
+import { useCallback, useState } from "react";
 
-type Func<T> = (...params: any[]) => Promise<T>;
+type Function = (...args: any[]) => Promise<any>;
 
-const wrapPromise = <T>(func: Func<T>, ...params: any[]) => {
-  var data: T;
+export const wrapPromise = <F extends Function>(func: F, ...params: Parameters<F>) => {
+  var data: Awaited<ReturnType<F>>;
 
   const suspender = func(...params).then(res => {
     data = res;
@@ -15,11 +15,14 @@ const wrapPromise = <T>(func: Func<T>, ...params: any[]) => {
   };
 };
 
-export const usePromise = <T>(func: Func<T>, ...params: any[]): [() => T, (...params: any[]) => void] => {
+export const usePromise = <F extends Function>(
+  func: F,
+  ...params: Parameters<F>
+): [() => Awaited<ReturnType<F>>, (...params: Parameters<F>) => void] => {
   const [reader, setReader] = useState(() => wrapPromise(func, ...params));
 
   const updater = useCallback(
-    (...newParams: any[]) => {
+    (...newParams: Parameters<F>) => {
       setReader(() => wrapPromise(func, ...newParams));
     },
     [func]
