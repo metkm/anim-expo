25c713d1f8113d51b8dc4cea73ed42cd63655f36
Sibyl <metinkorkmaz417@gmail.com>
2022-02-03 14:38:31 +0300

    change base button to RectButton from gesture handler

--- src/components/Base/Button.tsx
+++ src/components/Base/Button.tsx
@@ -1,50 +1,48 @@
 import React from "react";
-import { Text, StyleSheet, TouchableHighlight, TouchableHighlightProps, View } from "react-native";
+import { Text, StyleSheet, TouchableHighlightProps } from "react-native";
 import { useColors } from "../../hooks/useColors";
 
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { RectButton } from "react-native-gesture-handler";
 
 interface ButtonProps extends TouchableHighlightProps {
-  icon?: string
+  icon?: string;
+  onPress: () => void;
 }
 
 const Button = ({ onPress, style, children, icon }: ButtonProps) => {
   const { color } = useColors();
 
   return (
-    <TouchableHighlight
-      activeOpacity={0.9}
-      onPress={onPress}
-      style={styles.outer}
-    >
-      <View style={[styles.inner, { backgroundColor: color }, { ...style as {}}]}>
-        { icon && <Icon style={styles.icon} size={16} name={icon} color="white" /> }
-        <Text style={styles.text}>{children}</Text>
-      </View>
-    </TouchableHighlight>
+    <RectButton onPress={onPress} style={[styles.outer, { backgroundColor: color }, { ...(style as {}) }]}>
+      {icon && <Icon style={styles.icon} size={20} name={icon} color="white" />}
+      <Text style={styles.text}>{children}</Text>
+    </RectButton>
   );
 };
 
 const styles = StyleSheet.create({
-  inner: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 4,
-    padding: 8,
-  },
   outer: {
     margin: 2,
+    padding: 10,
+    borderRadius: 6,
+    flexDirection: "row",
+    alignItems: "center",
   },
   text: {
     color: "white",
-    fontFamily: "Overpass_700Bold",
+    flexGrow: 1,
     textAlign: "center",
-    flex: 1,
+    fontFamily: "Overpass_700Bold",
   },
   icon: {
+    position: "absolute",
+    left: 4,
+    top: 0,
+    bottom: 0,
     marginHorizontal: 4,
-  }
+    textAlignVertical: "center",
+  },
 });
 
 export default Button;
